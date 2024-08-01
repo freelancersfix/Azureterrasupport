4e814198ccd2b18d2a1dd608164afee528ced5b4
freelancersfix <freelancersfix@gmail.com>
2024-08-01 17:25:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
 #main.tf
-Required_providers {
-  azurerm = {
-     source = "hashicorp/azurerm"
-     version = "3.218.0"
- }
-  azuread = {
-    source = "hashicorp/azuread"
-    version = "2.15.0"
- }
-azapi = {
-  source = "hashicorp/azapi"
-  version = "1.15.0"
+terraform {
+  required_providers {
+     azurerm = {
+       source = "hashicorp/azurerm"
+       version = "3.218.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.15.0"
+    }
+    azapi = {
+     source = "hashicorp/azapi"
+     version = "1.15.0"
+   }
 }
 }
+
 
 provider "azurerm" {
    features {}
